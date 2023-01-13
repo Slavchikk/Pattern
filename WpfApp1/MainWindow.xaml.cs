973038b45c9ffb89a4a0c1a56e3ae70f59cfacd4
КарзиновВА <@106-10>
2023-01-13 17:44:52 +0300

    Добавьте файлы проекта.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Model.tb = TBCount;
+            Model.tbznak = TBznak;
+            Model.firstNum = firstNum;
+            Model.secomdNum = secondNum;
+            Model.tbResult = TbResult;
+            CBSurname.ItemsSource = Model.datalist;
+            TbResult.IsReadOnly = true;
+
+        }
+        private void OnPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = "0123456789 ,".IndexOf(e.Text) < 0;
+        }
+        private void BtnCount_Click(object sender, RoutedEventArgs e)
+        {
+            Model.operations(CBSurname.SelectedIndex);
+        }
+
+        private void CBSurname_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            Model.Combo = CBSurname.SelectedIndex;
+
+        }
+
+    }
+}
---
+++ WpfApp1/Model.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp1
+{
+    public static class Model
+    {// блок с данными
+        static int count = 0;
+        public static List<string> datalist = new List<string> { "Сложение", "Вычитание","Умножение", "Деление" };
+        public static List<string> dataZnak = new List<string> { "+", "-", "*", "/" };
+        // блок для обращения к текстовым полям
+        public static TextBlock tb;
+        public static TextBlock tbCombo;
+        public static TextBlock tbznak;
+        public static TextBox firstNum;
+        public static TextBox secomdNum;
+        public static TextBox tbResult;
+        // блок с бизнес-логикой
+        public static int Count // свойство для отображения числа в TextBlock
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+                tb.Text = count.ToString();
+            }
+        }
+        public static int Combo  // свойтсво для отображения фамилии в Combobox
+        {
+            set
+            {
+                tbznak.Text = dataZnak[value];
+            }
+        }
+        public static void operations(int i)
+        {
+            double firstCon = Double.Parse(firstNum.Text);
+            double secondCon = Double.Parse(secomdNum.Text);
+            double result;
+            switch (i)
+            {
+                case 0:
+                    result = firstCon+ secondCon;
+                    tbResult.Text = Convert.ToString(result);
+                    break;
+
+            }
+        }
+    }
+}
